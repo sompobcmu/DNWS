6e6222e44cf9f5d63db0b6782f9db517e433122b
sompobcmu <sompob_k@cmu.ac.th>
2019-05-18 11:15:36 +0700

    -add following and unfollowing -it still bug i can not login and go to tweeterapi in dotnet run.

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -12,6 +12,16 @@ angular.module('followingList', ['ngRoute'])
 
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
-      });
+		});
+		//create action_add for use in html
+		self.sendFollow = function sendFollow(followingname) {
+			$http.post('http://localhost:8080/twitterapi/following/', "followingname=" + encodeURIComponent(followingname), requestOptions);
+		}
+
+		//create action_delete for use in html
+		self.sendUnFollow = function sendUnFollow(followingname) {
+			$http.defaults.headers.delete = { 'X-session': $rootScope.x_session };
+			$http.delete('http://localhost:8080/twitterapi/following/?' + "followingname=" + encodeURIComponent(followingname));
+		}
     }]
 });
